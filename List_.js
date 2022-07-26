4ab0091068d083123155b99acb2709da1ff0b29a
guri <khalsagudeep@gmail.com>
2022-07-26 21:47:33 +0530

    feat
    Add features to view all list and rename list

--- List_.js
+++ List_.js
@@ -17,25 +17,51 @@ function list(){
             this.name=value;
         }
     });
-}
-let allLists =[];
 
-function CreateList(name,id){
-    allLists[id]=new list();
-    allLists[id].id = id;
-    allLists[id].name = name;
-    console.log(allLists[id].id , allLists[id].name);
 }
+
+function ListFeatures(){
+
+    this.allLists =[];
+
+    this.CreateList= (name,id)=> {
+        this.allLists[id]=new list();
+        this.allLists[id].id = id;
+        this.allLists[id].name = name;
+        //console.log(allLists[id].id , allLists[id].name);
+    }
+
+    this.veiwLists= () => {
+        for(key in this.allLists){
+            console.log(this.allLists[key].name);
+        }
+    }
+
+    this.veiwList= (id,name) => {
+        for(key in this.allLists){
+            if(this.allLists[key].id == id){
+                console.log();
+            }
+        }
+    }
+
+    this.RenameList= (id,name) =>{
+        for(key in this.allLists){
+            if(this.allLists[key].id == id){
+                this.allLists[key].name=name;
+            }
+        }
+    }
+}
+
+//testing
+
+let instance = new ListFeatures();
 let id =0;
-CreateList("today",id++);
-CreateList("Tomorrow",id++);
-
-// const list1 = new list();
-// list1.id=id++;
-// list1.name = "today";
-// console.log(list1.id , list1.name);
-
-// const list2 = new list();
-// list1.id=id++;
-// list1.name = "tommorow";
-// console.log(list1.id , list1.name);
+instance.CreateList("today",id++);
+instance.CreateList("Tomorrow",id++);
+instance.veiwLists();
+console.log(instance.allLists);
+instance.RenameList(1,"yesterday");
+instance.veiwLists();
+console.log(instance.allLists);
