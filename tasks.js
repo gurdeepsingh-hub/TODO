125efb131f24414ca781d4eda431268706b66441
guri <khalsagudeep@gmail.com>
2022-07-26 23:07:25 +0530

    feat
    Add features to create new tasks and edit content of existing task

---
+++ tasks.js
@@ -0,0 +1,67 @@
+function Task(listId){
+
+    this.name;
+    this.id;
+    this.listId=listId;
+    this.MarkTask= false;
+    this.content;
+
+    Object.defineProperties(this,{
+        TaskName:{
+            get: function(){
+                return this.name;
+            },
+            set: function(value){
+                this.name=value;
+            }
+        },
+        TaskId:{
+            get: function(){
+                return this.id;
+            },
+            set: function(value){
+                this.id= value;
+            }
+        },
+        TaskContent:{
+            get: function(){
+                return this.content;
+            },
+            set: function(value){
+                this.content=value;
+            }
+        },
+        TaskMark:{
+            get: function(){
+                return this.MarkTask;
+            },
+            set: function(value){
+                this.MarkTask=value;
+            }
+        }
+    });
+}
+
+function TaskFeatures(){
+    this.AllTasks = [[],[]];
+
+    this.CreateTask = (listId,name,taskId,content) =>{
+        this.AllTasks[listId][taskId]= new Task(listId);
+        this.AllTasks[listId][taskId].Taskid= taskId;
+        this.AllTasks[listId][taskId].TaskName = name;
+        this.AllTasks[listId][taskId].TaskContent= content;
+    }
+    this.EditContent = (listId,taskId) =>{
+        for( [listId][taskId] in this.AllTasks){
+
+            console.log(this.AllTasks[listId][taskId]);
+        }
+    }
+
+}
+
+//testing
+const instance = new TaskFeatures();
+instance.CreateTask(0,"complete_project",1,"complete todo list app");
+instance.CreateTask(0,"complete_project1",2,"complete todo list app1");
+instance.EditContent(0,1);
